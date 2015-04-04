f91c9c0829af089a925bf8c436c0abb967d7b6a2
Gastón I. Silva E <silvag777@gmail.com>
2015-04-04 16:50:51 -0600

    bump ember-cli and ember-data

--- Brocfile.js
+++ Brocfile.js
@@ -17,7 +17,7 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-if (app.env === 'development') {
+if (app.env !== 'production') {
   app.import('bower_components/jquery.isinview/dist/jquery.isinview.js');
 }
 
